555d88a99774828a5b3ae02e1c7b758d55834fcd
GonzalezGabriel-dev <gabrielgonzalez9913@gmail.com>
2022-01-07 12:45:29 -0700

    fix function

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,6 +1,9 @@
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Allow from anywhere
+    },
     body: JSON.stringify({ message: 'Hello World!!' }),
   };
 };
