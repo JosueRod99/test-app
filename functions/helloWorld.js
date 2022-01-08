feeb0a85605bd67febdaa88cfe94d47d6151cb04
GonzalezGabriel-dev <gabrielgonzalez9913@gmail.com>
2022-01-08 12:57:50 -0700

    new conection

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,9 +1,24 @@
 exports.handler = async function (event, context) {
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host: 'tvozlishe0l0.us-east-1.psdb.cloud',
+    user: 'bcfsah6y61ik',
+    password: 'pscale_pw_7nP4KruYwmDeebrl01vQQdOT4jsNAAqomJPR_W3L9v8',
+  });
+  var message = '';
+  connection.connect(function (err) {
+    if (err) {
+      message = 'error connecting: ' + err.stack;
+      return;
+    }
+
+    message = 'connected as id ' + connection.threadId;
+  });
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Allow from anywhere
     },
-    body: JSON.stringify({ message: 'Hello World!!' }),
+    body: JSON.stringify({ message }),
   };
 };
