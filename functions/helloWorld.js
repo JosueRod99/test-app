26aada48b2d656967b7f2cb7573f996e503b34fe
GonzalezGabriel-dev <gabrielgonzalez9913@gmail.com>
2022-01-10 12:30:23 -0700

    renew function14

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,37 +1,26 @@
-// get the client
-const mysql = require('mysql2');
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'tvozlishe0l0.us-east-1.psdb.cloud',
-  user: 'bcfsah6y61ik',
-  password: 'pscale_pw_7nP4KruYwmDeebrl01vQQdOT4jsNAAqomJPR_W3L9v8',
-  database: 'example',
-  ssl: {
-    rejectUnauthorized: true,
+const mysql = require('serverless-mysql')({
+  config: {
+    host: process.env.ENDPOINT,
+    database: process.env.DATABASE,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
   },
 });
+// Main handler function
+exports.handler = async (event, context) => {
+  // Run your query
+  let results = await mysql.query('SELECT * FROM table');
 
-exports.handler = async function (event, context, text) {
-  // simple query
-  var someVar = [];
-  function setValue(value) {
-    someVar = value;
-  }
-  connection.connect();
-  connection.query('SELECT * FROM `users`', function (err, results, fields) {
-    if (err) {
-      throw err;
-    } else {
-      setValue(results);
-    }
-  });
+  // Run clean up function
+  await mysql.end();
+
+  // Return the results
 
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Allow from anywhere
     },
-    body: JSON.stringify({ message: someVar }),
+    body: JSON.stringify({ message: results }),
   };
 };
